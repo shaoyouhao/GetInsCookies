86650d47661a035bc68b91ce451d08d17f20ebd7
youhao <shaoyouhao@duoshoubang.cn>
2021-05-25 16:09:05 +0800

    写入mysql的信息

--- setting.py
+++ setting.py
@@ -3,5 +3,12 @@ import os
 # 浏览器的状态, False隐藏窗口
 has_screen = False
 
-# 数据库的连接
+# mongoDB数据库的连接
 URI = os.environ.get("REMOTE_DB", "")
+
+# mysql数据连接
+HOST = "xxx.xxx.xxx.xxx"
+USER = "root"
+PASSWORD = "xxx"
+DB = "xxx"
+PORT = 3306
