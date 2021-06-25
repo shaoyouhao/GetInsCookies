7bfdda6ff65e481fe8f21b669c8ab559bfd103be
youhao <shaoyouhao@duoshoubang.cn>
2021-06-25 13:33:57 +0800

    默认不使用mongo数据库

--- setting.py
+++ setting.py
@@ -4,17 +4,12 @@ import os
 has_screen = False
 
 # mongoDB数据库的连接
-<<<<<<< HEAD
 ISMONGO = False
 URI = os.environ.get("REMOTE_DB", "")
-=======
-ISMONGO = False  # 是否使用mongo数据库
-URI = os.environ.get("REMOTE_DB", "mongodb://crawler:Duoshoubang@120.55.84.175/dsb")
->>>>>>> bb529a568e47bd79e9c39c5122e6e849f87d41d0
 
 # mysql数据连接
-HOST = "112.124.40.161"
+HOST = "xxx.xxx.xxx.xxx"
 USER = "root"
-PASSWORD = "DevOps2021!"
-DB = "kol_analysis"
+PASSWORD = "xxx"
+DB = "xxx"
 PORT = 3306
